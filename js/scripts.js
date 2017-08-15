920506b56cd25d89aa2e372eee282487d871ebf6
kwiatkowskaela <kwiatkowska.grafik@gmail.com>
2017-08-15 12:17:44 +0200

    zmiana js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var withButtonItemsClass = document.getElementsByClassName('button');
+
+for (var i = 0; withButtonItemsClass.length; i++) {
+    alert(withButtonItemsClass[i].innerText);
+    console.log(withButtonItemsClass[i].innerText);
+}
